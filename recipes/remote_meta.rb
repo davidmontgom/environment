0cb6e34283888496e3f1de5af5cde7b29e72ffc2
David Montgomery <davidmontgomery@gmail.com>
2016-01-09 05:05:48 +0800

    update

--- recipes/remote_meta.rb
+++ recipes/remote_meta.rb
@@ -1,8 +1,8 @@
 datacenter = node.name.split('-')[0]
-location = node.name.split('-')[1]
-environment = node.name.split('-')[2]
-slug = node.name.split('-')[3] 
-server_type = node.name.split('-')[4]
+environment = node.name.split('-')[1]
+location = node.name.split('-')[2]
+server_type = node.name.split('-')[3]
+slug = node.name.split('-')[4] 
 
 data_bag("meta_data_bag")
 aws = data_bag_item("meta_data_bag", "aws")
@@ -10,35 +10,11 @@ AWS_ACCESS_KEY_ID = aws[node.chef_environment]['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = aws[node.chef_environment]['AWS_SECRET_ACCESS_KEY']
 
 
-directory "/etc/ec2" do
-  owner "root"
-  group "root"
-  mode "0755"
-  recursive true
-  action :create
-end
-
-=begin
-if datacenter == "aws"
-  cookbook_file "/etc/ec2/#{datacenter}_#{node.chef_environment}_#{location}.pem" do
-    source "#{datacenter}_#{node.chef_environment}_#{location}.pem"
-    mode 0600
-  end
-end
-=end
-
-file "/etc/ec2/#{node.chef_environment}" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
-end
-
 class_path = node['environment']['class_path'] 
 settings_path = "/var/#{slug}-settings"
 
-template "/etc/ec2/meta_data.yaml" do
-  path "/etc/ec2/meta_data.yaml"
+template "/root/.bootops.yaml" do
+  path "/root/.bootops.yaml"
   source "meta_data.yaml.erb"
   owner "root"
   group "root"
