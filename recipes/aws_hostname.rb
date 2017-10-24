3cac722a5ebbfae835d0382cb53e51293af5b4c5
Your Name <you@example.com>
2017-10-25 05:36:59 +0800

    update

--- recipes/aws_hostname.rb
+++ recipes/aws_hostname.rb
@@ -1,19 +1,39 @@
 
 nodename = node.name
+if node.platform_version.to_f == 14.04
+	if node.chef_environment!="local"
+		bash "aws_hostname" do
+		  user "root" 
+		  group "root"
+		  code <<-EOH
+		      echo '127.0.1.1 #{nodename} #{nodename}' | tee -a /etc/hosts
+		      > /etc/hostname
+		      echo '#{nodename}' | tee -a /etc/hostname
+		      service networking restart
+		      service hostname restart
+		      touch /var/chef/cache/aws_hostname
+		  EOH
+		  action :run
+		  not_if {File.exists?("/var/chef/cache/aws_hostname")}
+		end
+	end
+end
 
-if node.chef_environment!="local"
-	bash "aws_hostname" do
-	  user "root" 
-	  group "root"
-	  code <<-EOH
-	      echo '127.0.1.1 #{nodename} #{nodename}' | tee -a /etc/hosts
-	      > /etc/hostname
-	      echo '#{nodename}' | tee -a /etc/hostname
-	      service networking restart
-	      service hostname restart
-	      touch /var/chef/cache/aws_hostname
-	  EOH
-	  action :run
-	  not_if {File.exists?("/var/chef/cache/aws_hostname")}
+if node.platform_version.to_f == 16.04
+	if node.chef_environment!="local"
+		bash "aws_hostname" do
+		  user "root" 
+		  group "root"
+		  code <<-EOH
+		      echo '127.0.1.1 #{nodename} #{nodename}' | tee -a /etc/hosts
+		      > /etc/hostname
+		      echo '#{nodename}' | tee -a /etc/hostname
+		      hostnamectl set-hostname #{nodename}
+		      systemctl restart systemd-logind.service
+		      touch /var/chef/cache/aws_hostname
+		  EOH
+		  action :run
+		  not_if {File.exists?("/var/chef/cache/aws_hostname")}
+		end
 	end
 end
