5d8d25314a9aac75cd51791b73d9492f18fc3ecc
David Montgomery <davidmontgomery@gmail.com>
2018-10-24 14:30:50 +0800

    update

--- recipes/aws_hostname.rb
+++ recipes/aws_hostname.rb
@@ -37,3 +37,22 @@ if node['platform_version'].to_f == 16.04
 		end
 	end
 end
+
+if node['platform_version'].to_f == 18.04
+	if node.chef_environment!="local"
+		bash "aws_hostname" do
+		  user "root" 
+		  group "root"
+		  code <<-EOH
+		      echo '127.0.1.1 #{nodename} #{nodename}' | tee -a /etc/hosts
+		      > /etc/hostname
+		      echo '#{nodename}' | tee -a /etc/hostname
+		      hostnamectl set-hostname #{nodename}
+		      systemctl restart systemd-logind.service
+		      touch /var/chef/cache/aws_hostname
+		  EOH
+		  action :run
+		  not_if {File.exists?("/var/chef/cache/aws_hostname")}
+		end
+	end
+end
---
+++ recipes/nano.rb
@@ -0,0 +1,4 @@
+https://askubuntu.com/questions/73444/how-to-show-line-numbering-in-nano-when-opening-a-file
+https://askubuntu.com/questions/770887/how-to-display-line-numbers-in-nano-text-editor
+~/.nanorc
+set const
